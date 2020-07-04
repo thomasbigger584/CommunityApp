26888217962a6b990fada1fc038949112811ce90
Bigger, Thomas <thomas@performanceactive.com>
2020-07-04 16:09:59 +0100

    first commit

---
+++ src/sagas/usersSagas.ts
@@ -0,0 +1,18 @@
+import * as types from '../actions/actionTypes';
+import {fetchUserFailure, fetchUserSuccess} from '../actions/usersActions';
+import JSONPlaceholderAPI from '../lib/jsonPlaceholderAPI';
+import {call, put, takeEvery} from 'redux-saga/effects';
+import {BaseAction} from '../types';
+
+function* fetchUser(action: BaseAction) {
+  try {
+    const user = yield call(JSONPlaceholderAPI.fetchUser, action.payload);
+    yield put(fetchUserSuccess(user));
+  } catch (e) {
+    yield put(fetchUserFailure(e.message));
+  }
+}
+
+export default function* root() {
+  yield takeEvery(types.USER.FETCH, fetchUser);
+}
