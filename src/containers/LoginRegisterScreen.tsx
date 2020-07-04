f27fcc47dc98ecf27b8fee4c8e02c5a0395ea059
Bigger, Thomas <thomas@performanceactive.com>
2020-07-04 21:59:28 +0100

    added some styling

--- src/containers/LoginRegisterScreen.tsx
+++ src/containers/LoginRegisterScreen.tsx
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {Button, StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {RootStackParamList} from '../App';
 import SplashScreen from 'react-native-splash-screen'
 import {StackNavigationProp} from "@react-navigation/stack";
 import AppLinearGradientBackground from "../components/AppLinearGradientBackground";
+import {AppButton} from "../components/AppButton";
 
 interface LoginRegisterScreenProps {
     navigation: StackNavigationProp<RootStackParamList, 'LoginRegister'>;
@@ -13,37 +14,29 @@ export class LoginRegisterScreen extends Component<LoginRegisterScreenProps> {
     componentDidMount() {
         SplashScreen.hide();
     }
-
     render() {
         return (
             <AppLinearGradientBackground>
-                <Text style={styles.buttonText}>
-                    Sign in with Facebook
-                </Text>
-
-                <Button
-                    title="Login"
-                    onPress={() => this.props.navigation.navigate('Login', {})}
-                />
-
-                <Button
-                    title="Register"
-                    onPress={() => this.props.navigation.navigate('Register', {})}
-                />
+                <View style={styles.container}>
+                    <AppButton title="Login" onPress={() => {
+                        this.props.navigation.navigate('Login', {})
+                    }}/>
+                    <AppButton title="Register" onPress={() => {
+                        this.props.navigation.navigate('Register', {})
+                    }}/>
+                </View>
             </AppLinearGradientBackground>
         );
     }
 }
-
 export default LoginRegisterScreen;
 
 const styles = StyleSheet.create({
-    buttonText: {
-        fontSize: 18,
-        fontFamily: 'Gill Sans',
-        textAlign: 'center',
-        margin: 10,
-        color: '#ffffff',
-        backgroundColor: 'transparent',
+    container: {
+        flex: 1,
+        flexDirection: "column-reverse",
+        marginLeft: 50,
+        marginRight: 50,
+        justifyContent: "center"
     },
 });
